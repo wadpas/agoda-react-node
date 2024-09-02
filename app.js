8907996d98a98b955aabba41d74260b43e722ce7
wadpas <wadpas@gmail.com>
2024-09-02 09:49:28 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+const start = async () => {
+	try {
+		app.listen(port, () => {
+			console.log('Server started on port 3000')
+		})
+	} catch (error) {}
+}
+
+start()
